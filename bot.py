7276e4473ceb62088b42869f812a1a50f2a902f8
DrDrunkenstein42 <omkys.potatofries@gmail.com>
2021-11-16 21:28:54 +0530

    Bot structure added

--- bot.py
+++ bot.py
@@ -1,8 +1,18 @@
 import discord
+import firebase_admin
+from firebase_admin import db, credentials
+
 
 '''
 THIS IS THE MAIN FILE FOR THE BOT
 '''
+
+# Reference to database where user data is stored
+cred_obj = credentials.Certificate('./discord-casino-bot-firebase-adminsdk-xctx7-e413fd8a52.json')
+default_app = firebase_admin.initialize_app(cred_obj, {
+	    'databaseURL':'https://discord-casino-bot-default-rtdb.asia-southeast1.firebasedatabase.app/'
+	})
+ref = db.reference('/')
 
 client = discord.Client()
 TOKEN = "OTEwMTkxOTY4NDMxMjQ3NDQw.YZPQUw.8lobqubSVdeeVL9OCWeL5BhKjfE"
@@ -18,4 +28,3 @@ async def on_connect():
 async def on_message(message):
     if message.content.startswith('!help'):
         await client.send_message(message.channel, help_message)
-
