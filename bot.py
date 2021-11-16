65060ea2f2bea1e2a9d861e339984cf42a40ab7a
DrDrunkenstein42 <omkys.potatofries@gmail.com>
2021-11-16 21:13:29 +0530

    Bot set up

--- bot.py
+++ bot.py
@@ -3,3 +3,19 @@ import discord
 '''
 THIS IS THE MAIN FILE FOR THE BOT
 '''
+
+client = discord.Client()
+TOKEN = "OTEwMTkxOTY4NDMxMjQ3NDQw.YZPQUw.8lobqubSVdeeVL9OCWeL5BhKjfE"
+
+help_message = "Work in Progress."
+
+@client.event
+async def on_connect():
+    print("Connected.")
+    
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!help'):
+        await client.send_message(message.channel, help_message)
+
