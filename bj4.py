f12fd6636f55c4265c002f8befaa88f0fc850d96
char2539 <gvenkatasreecharan@gmail.com>
2022-01-20 18:05:43 +0530

    Bj added

--- bj4.py
+++ bj4.py
@@ -1,7 +1,7 @@
 import random
 
 suits = ["Spades", "Hearts", "Clubs", "Diamonds"] 
-suits_values = {"Spades":"\u2664", "Hearts":"\u2661", "Clubs": "\u2667", "Diamonds": "\u2662"}
+suits_values = {"Spades":'♠', "Hearts":'♥', "Clubs": '♣', "Diamonds": '♦'}
 cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 card_values = {"A": 11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8,
                 "9":9, "10":10, "J":10, "Q":10, "K":10}
