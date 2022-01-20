fadbf70159dfa4584917993b3119ef35f7986fb4
DrDrunkenstein42 <omkys.potatofries@gmail.com>
2022-01-20 11:43:06 +0530

    Poker functionality added

--- src/blackjack/global_vars.py
+++
@@ -1,7 +0,0 @@
-#There's likely a much better way to do this
-global tracked_players
-global ingame_channels
-#Store player records in <player id> : Player() pairs.
-tracked_players = {}
-#Store games in <channel id> : Game() pairs
-ingame_channels = {}
